9589cf85673d60e0f318764c36778de373ef52e8
rotyipi <zaitianqingji@gmail.com>
2026-05-22 11:46:33 +0900

    P84(故意エラー)

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
-function getCat(catName){
-    const res=`猫名：「${catName}」`;
-    return res;
+function outFunc(){
+    function getCat(catName){
+        const res=`猫名：「${catName}」`;
+        return res;
+    }
+
+    console.log(getCat("タマ"));
 }
 
-console.log(getCat("タマ"));
+outFunc();
+console.log(getCat("大福"));
