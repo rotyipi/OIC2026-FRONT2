970b9184a16f21b0fe0f6e47ab694ae62475c11c
rotyipi <zaitianqingji@gmail.com>
2026-05-22 10:56:16 +0900

    P69-2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
-const cat={name:"タマ",age:3,color:"black"};
+const animal={
+    cat:{name:"タマ",age:3,color:"black"},
+    dog:{name:"ポチ",age:4,color:"white"}
+};
 
-console.log(cat.name);
-console.log(cat["age"]);
+console.log(animal.cat.name);
+console.log(animal["cat"]["age"]);
+
+console.log(animal["dog"].color);
+console.log(animal.dog["color"])
