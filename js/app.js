3a80a4baace855e5e6551828ea485a30a26f8da0
rotyipi <zaitianqingji@gmail.com>
2026-05-22 10:32:17 +0900

    P64

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
-//P63
+//P64
 console.log("start");
 
 {
     let item="チョコレート"
-    console.log(item);
+    console.log("1-1",item);
     {
-        console.log(item)
+        let item="キャンディー";
+        console.log("2-1",item);
+
+        {
+            console.log("3-1",item);
+        }
+        console.log("2-2",item);
     }
+    console.log("1-2",item);
 }
 console.log("end")
